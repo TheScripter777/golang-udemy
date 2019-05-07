cbcb80e3d754be76a7cba10a183b525da851fd4c
Guillaume Rivest <guillaume.rivest@desjardins.com>
2019-05-07 08:17:15 -0400

    Section 7, hands-on exercise #6

---
+++ Section7/Hands-on_Exercise6/main.go
@@ -0,0 +1,22 @@
+/**
+Section 7 -- 45. Hands-on exercise #4
+Using iota, create 4 constants for the last 4 years. Print the constant values
+*/
+package main
+
+import "fmt"
+
+const (
+	_ = iota
+	a = 2019 - iota
+	b = 2019 - iota
+	c = 2019 - iota
+	d = 2019 - iota
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
