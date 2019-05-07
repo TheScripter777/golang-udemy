da09f0e8238b9e5c96ca25661b36967fbb0116ee
Guillaume Rivest <guillaume.rivest@desjardins.com>
2019-05-07 08:12:24 -0400

    Section 7, hands-on exercise #5

---
+++ Section7/Hands-on_Exercise5/main.go
@@ -0,0 +1,15 @@
+/**
+Section 7 -- 45. Hands-on exercise #4
+Create a variable of type string using a raw string literal. Print it.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	s := `This is a "raw" string
+
+litteral`
+
+	fmt.Println(s)
+}
