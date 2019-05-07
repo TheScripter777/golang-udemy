91fa258a7bd88f4c7e912168a118ef68d62af714
Guillaume Rivest <guillaume.rivest@desjardins.com>
2019-05-07 07:53:30 -0400

    Section 7, hands-on exercise #1

---
+++ Section7/Hands-on_Exercise1/main.go
@@ -0,0 +1,18 @@
+/**
+Section 7 -- 42. Hands-on exercise #1
+Write a program that prints a number in decimal, binary and hex
+ */
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+
+	fmt.Printf("Decimal: %d\n", x)
+	fmt.Printf("Binary: %b\n", x)
+	fmt.Printf("Hexadecimal: %#X\n", x)
+
+
+
+}
